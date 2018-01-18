b9b5b6a404853699d4266f1ce7f1ace2ea79aa6e
Tom Gardham-Pallister <tom.pallister@fourth.com>
2018-01-18 19:32:36 +0000

    exercise screen

---
+++ Calm/app/src/main/java/com/moonpig/calm/calm/Exercise.java
@@ -0,0 +1,15 @@
+package com.moonpig.calm.calm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Exercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+    }
+}
