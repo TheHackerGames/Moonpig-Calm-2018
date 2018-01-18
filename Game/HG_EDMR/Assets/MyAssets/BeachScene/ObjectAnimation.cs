7f04c5cc277d17fe16ab897ce878ec1d5e577752
Darren Beukes <darren.beukes@gmail.com>
2018-01-18 20:14:27 +0000

    Rotation on ball

---
+++ Game/HG_EDMR/Assets/MyAssets/BeachScene/ObjectAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectAnimation : MonoBehaviour {
+
+	public AnimationCurve curve;
+
+	void Start()
+	{
+		curve = new AnimationCurve(
+			new Keyframe(0, 0), 
+			new Keyframe(1, 1)
+		);
+		curve.preWrapMode = WrapMode.PingPong;
+		curve.postWrapMode = WrapMode.PingPong;
+	}
+
+	void Update()
+	{
+//		transform.position = new Vector3(transform.position.x, curve.Evaluate(Time.time), transform.position.z);
+//		transform.position = new Vector3(curve.Evaluate(Time.time), transform.position.y, transform.position.z);
+		transform.position = new Vector3(curve.Evaluate(Time.deltaTime), transform.position.y, transform.position.z);
+	}
+}
