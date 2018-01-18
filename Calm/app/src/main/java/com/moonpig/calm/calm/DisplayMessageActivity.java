95f3fb55d4138ec481e1cacacf58c9a41e8950b9
Tom Gardham-Pallister <tom.pallister@fourth.com>
2018-01-18 11:16:59 +0000

    stuff

---
+++ Calm/app/src/main/java/com/moonpig/calm/calm/DisplayMessageActivity.java
@@ -0,0 +1,18 @@
+package com.moonpig.calm.calm;
+import com.moonpig.game.UnityPlayerActivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = new Intent(this, UnityPlayerActivity.class);
+        startActivity(intent);
+    }
+}
